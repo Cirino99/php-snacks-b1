3427c124916127a8d284b464e68c77c4a53e6656
Simone <cirino.simone99@gmail.com>
2022-06-27 17:05:00 +0200

    finito terzo snack (senza grafica)

---
+++ snack3.php
@@ -0,0 +1,79 @@
+<?php
+$posts = [
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+$date = array_keys($posts);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <section>
+        <?php
+        for ($i = 0; $i < count($posts); $i++) {
+            $post = $posts[$date[$i]]; ?>
+            <div class="d-flex">
+                <div>
+                    <span><?= $date[$i] ?></span>
+                </div>
+                <div class="d-flex ms-4">
+                    <?php
+                    for ($j = 0; $j < count($post); $j++) { ?>
+                        <div class="me-4">
+                            <span><?= $post[$j]['author'] ?></span>
+                            <span><?= $post[$j]['title'] ?></span>
+                            <p><?= $post[$j]['text'] ?></p>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        <?php } ?>
+    </section>
+</body>
+
+</html>
